a4ee6bbc16a0506779fdc93c27c4af9da72851e1
Mark Canlas <m4rkd0tc0de@gmail.com>
2022-06-29 02:02:51 +0800

    change name to index html and modifying oop function

--- oop.js
+++ oop.js
@@ -1,18 +1,25 @@
 'use strict';
 
-const Person = function(firstName, birthYear) {
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
 
-
+  }
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
 }
 
 const Mark = new Person('Mark', 1992);
+const Anel = new Person('Mark', 1991);
 
 console.log(Mark);
+console.log(Anel);
 console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-}
 Mark.calcAge();
+Anel.calcAge();
+console.log(Mark.hasOwnProperty('firstName'))
+
+console.log(Person.prototype.constructor);
