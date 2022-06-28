4800ab2135a2f8396c8ed59ad88ab96bf0e70801
Mark Canlas <m4rkd0tc0de@gmail.com>
2022-06-29 02:03:39 +0800

    adding setters and getters function

---
+++ settersAndGetters.js
@@ -0,0 +1,42 @@
+
+//Object setters and getters
+const account = {
+  owner: 'Mark',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov)
+  }
+}
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
+
+
+//Class setters and getters
+class Person {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  get age() {
+    return 2037 - this.birthYear
+  }
+  set fullName(name) {
+    if(name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`)
+  }
+  get fullName() {
+    return this._fullName;
+  }
+}
+
+const Mark = new Person('Mark Canlas', 1992)
+
+console.log(Mark.age);
